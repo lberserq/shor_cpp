955f26d3a8b4a8ccabdd1257d6378d9c07b2b35a
Kamil Khamitov <berserq0123@gmail.com>
2015-03-25 12:30:41 +0300

    noise added

---
+++ qft_adder.cpp
@@ -0,0 +1,12 @@
+#include "qft_adder.h"
+
+qft_adder::qft_adder()
+{
+
+}
+
+qft_adder::~qft_adder()
+{
+
+}
+
---
+++ qft_adder.h
@@ -0,0 +1,12 @@
+#ifndef QFT_ADDER_H
+#define QFT_ADDER_H
+
+
+class qft_adder
+{
+public:
+    qft_adder();
+    ~qft_adder();
+};
+
+#endif // QFT_ADDER_H
